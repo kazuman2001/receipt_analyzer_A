0decae954698dcbd9efc84a47063063110d036a4
kazuman2001 <kazuman2001@gmail.com>
2025-01-21 13:42:09 +0900

    領収証アナライザAのドキュメントを更新し、PDFファイルの処理機能を改善しました。ユーザーにリネームしたPDFファイルの複製を確認する機能を追加し、サブフォルダ内のPDFファイルも処理できるようにしました。

--- receipt_analyzer_A.py
+++ receipt_analyzer_A.py
@@ -1,20 +1,43 @@
-# 領収証アナライザA
-# 機能
-# - りんご領収証PDFファイルを読み込み、抽出したデータをCSVファイルに保存する
-# - CSVを"注文日"・"注文番号"の順で昇順にソートする
-# - "input"フォルダに保存された複数のPDFファイルを順次読み込む
-# - "output"フォルダにリネームしたPDFファイルを保存する
-#   yyyy-mm-dd_領収証_仕入_<店舗名>_<決済額1>円_<決済2><決済額2>円_<決済3><決済額3>円_<注文番号>.pdf
-# - ERROR発生時にtext化したPDFファイルをerror_YYYYMMDD_hhmm.txtに保存する
-# CSVに保存するデータ：
-# - １列目から[注文番号,請求番号,注文日,注文月,請求日,請求金額,請求先,配送先,モール名,リネーム前ファイル名,リネーム後ファイル名,領収証内情報]
+"""
+領収証アナライザA
 
+このスクリプトは、りんご領収証PDFファイルを読み込み、抽出したデータをCSVファイルに保存します。
+また、CSVを"注文日"・"注文番号"の順で昇順にソートし、"input"フォルダに保存された複数のPDFファイルを順次読み込みます。
+さらに、"output"フォルダにリネームしたPDFファイルを保存します。
+リネーム形式は以下の通りです：
+yyyy-mm-dd_領収証_仕入_<店舗名>_<決済額1>円_<決済2><決済額2>円_<決済3><決済額3>円_<注文番号>.pdf
+ERROR発生時には、text化したPDFファイルをerror_YYYYMMDD_hhmm.txtに保存します。
+
+CSVに保存するデータ：
+- 注文番号
+- 請求番号
+- 注文日
+- 注文月
+- 請求日
+- 請求金額
+- 請求先
+- 配送先
+- モール名
+- リネーム前ファイル名
+- リネーム後ファイル名
+- 領収証内情報
+
+関数:
+- extract_text_from_pdf(pdf_path): PDFファイルからテキストを抽出し、正規化したテキストを返します。
+- initialize_data_structure(): データを保存するための初期構造を返します。
+- extract_order_number(text): テキストから注文番号を抽出します。
+- extract_billing_number(text): テキストから請求番号を抽出します。
+- extract_purchase_date_and_month(text): テキストから注文日と注文月を抽出します。
+- copyfile_to_output_folder(pdf_path, new_pdf_file): リネーム後のPDFファイルを元のフォルダ構成でoutputフォルダに保存します。
+"""
 
 import os
 import unicodedata
 import shutil
 import pypdf
 import pandas as pd
+import ctypes
+
 
 def extract_text_from_pdf(pdf_path):
     with open(pdf_path, 'rb') as file:
@@ -105,9 +128,7 @@ def extract_total_price(text):
 def extract_billing_name(text):
     # "請求先"の次の行の"様"から改行までを請求先として抽出
     if '請求先' in text and '様' in text:
-        # billing_name = text.split('請求先')[1].split('様')[0].split('\n')[0]
         billing_name = text.split('請求先')[1].split('\n')[5].split('様')[0]
-
     else:
         billing_name = 'ERROR'
 
@@ -171,8 +192,8 @@ def parse_pdf_text(text):
     return data, isError
 
 
-# リネーム後ファイル名を生成（決済2、決済3が空の場合は空白にする）
 def generate_new_pdf_file_name(data):
+    # リネーム後ファイル名の形式：yyyy-mm-dd_領収証_仕入_<店舗名>_<決済額1>円_<決済2><決済額2>円_<決済3><決済額3>円_<注文番号>.pdf
     if 'ERROR' in data['注文番号'][-1] or 'ERROR' in data['請求番号'][-1] or 'ERROR' in data['注文日'][-1] or 'ERROR' in data['請求金額'][-1]:
         return 'ERROR'
     
@@ -183,6 +204,19 @@ def generate_new_pdf_file_name(data):
     return new_pdf_file
 
 
+def copyfile_to_output_folder(pdf_path, new_pdf_file):
+    output_folder = 'output'
+    input_folder = 'input'
+    relative_path = os.path.relpath(pdf_path, input_folder)
+    output_path = os.path.join(output_folder, os.path.dirname(relative_path))
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    if new_pdf_file != 'ERROR': 
+        shutil.copyfile(pdf_path, os.path.join(output_path, new_pdf_file))
+        
+
 def sort_data(data):
     # データを"購入日"・"注文番号"の順でソート
     df = pd.DataFrame(data)
@@ -191,6 +225,7 @@ def sort_data(data):
 
 
 def save_to_csv(data, output_file):
+    # データをCSVファイルに保存
     df = pd.DataFrame(data)
     df.to_csv(output_file, index=False, encoding='utf-8_sig')
 
@@ -199,12 +234,30 @@ def main():
     input_folder = 'input'
     pdf_text_with_error = ''
     
-    # フォルダ内のPDFファイルを取得
-    pdf_files = [file for file in os.listdir(input_folder) if file.endswith('.pdf')]
+    # ユーザーにリネームしたPDFファイルを複製するか尋ねる
+    user_response = ctypes.windll.user32.MessageBoxW(0, "リネーム.pdfを複製しますか？\n\n「はい」を選ぶとリスト作成とPDF複製の両方を行います\n「いいえ」を選ぶとリスト作成のみ行います", "確認", 4)
+    copy_file_select = 'Yes' if user_response == 6 else 'No'
+    
+    # 処理中メッセージを表示
+    print('処理中... このウィンドウを閉じないでください。')
 
     # CSVに保存するデータを初期化
     all_data = initialize_data_structure()
 
+    # input_folder直下のPDFファイルを取得
+    pdf_files = [file for file in os.listdir(input_folder) if file.endswith('.pdf')]
+
+    # input_folder直下のサブフォルダを取得
+    # サブフォルダは1階層のみ対応
+    subfolders = [folder for folder in os.listdir(input_folder) 
+                  if os.path.isdir(os.path.join(input_folder, folder))]
+
+    # サブフォルダ内のPDFファイルを取得
+    for subfolder in subfolders:
+        subfolder_path = os.path.join(input_folder, subfolder)
+        subfolder_pdf_files = [file for file in os.listdir(subfolder_path) if file.endswith('.pdf')]
+        pdf_files.extend([os.path.join(subfolder, file) for file in subfolder_pdf_files])
+        
     # 各PDFファイルを順次読み込んでデータを抽出
     for pdf_file in pdf_files:
         pdf_path = os.path.join(input_folder, pdf_file) # PDFファイルのパスを取得
@@ -217,14 +270,10 @@ def main():
         new_pdf_file = generate_new_pdf_file_name(data) # リネーム後ファイル名を生成
         data['リネーム後ファイル名'].append(new_pdf_file)   # リネーム後ファイル名をCSVに保存
         
-        # outputフォルダにリネームしたPDFファイルを保存
-        output_folder = 'output'
-        if not os.path.exists(output_folder):
-            os.makedirs(output_folder)
         # outputフォルダにリネームしたPDFファイルをコピーして保存
-        if new_pdf_file != 'ERROR':
-            shutil.copyfile(pdf_path, os.path.join(output_folder, new_pdf_file))
-        
+        if copy_file_select == 'Yes':
+            copyfile_to_output_folder(pdf_path, new_pdf_file)
+            
         for key in all_data:
             all_data[key].extend(data[key])
 
@@ -241,8 +290,10 @@ def main():
         with open(error_file, 'w', encoding='utf-8_sig') as f:
             f.write(pdf_text_with_error)  
             
-    # Enterを押すとコンソールを閉じる
-    input('処理が完了しました。Enterを押してください。')
+    # 処理完了メッセージを表示
+    print('...処理が完了しました。')
+    # 処理完了をメッセージボックスで通知
+    ctypes.windll.user32.MessageBoxW(0, "処理が完了しました。", "完了", 0)
 
 if __name__ == "__main__":
     main()
